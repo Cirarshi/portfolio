9311c5c118177ea3fea0a3b40150e3fc5f7997b7
Utkarsh Kapoor <98.utkarsh.kapoor@gmail.com>
2025-05-21 01:20:12 +0530

    v3
    Layout of Skills

---
+++ src/components/ParticlesBackground.jsx
@@ -0,0 +1,66 @@
+// ParticlesBackground.jsx
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+function ParticlesBackground() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        background: {
+          color: {
+            value: "#000000", // Black background
+          },
+        },
+        particles: {
+          number: {
+            value: 50,
+            density: {
+              enable: true,
+              area: 800,
+            },
+          },
+          color: {
+            value: "#ffffff", // White particles
+          },
+          shape: {
+            type: "circle",
+          },
+          opacity: {
+            value: 0.5,
+          },
+          size: {
+            value: 3,
+            random: true,
+          },
+          move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            outModes: {
+              default: "out",
+            },
+          },
+        },
+        interactivity: {
+          events: {
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
+
+export default ParticlesBackground;
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,10 +1,73 @@
+//import ParticlesBackground from "../components/ParticlesBackground";
 import PageNav from "../components/PageNav";
+import styles from "./Skills.module.css";
+//import { FaHtml5, FaCss3Alt, FaJs, FaReact } from "react-icons/fa";
+import html5 from "../assets/html.png";
+import css3 from "../assets/css3.png";
+import js from "../assets/java-script.png";
+import jquery from "../assets/jquery.png";
+import reactLogo from "../assets/react.png";
+import typescript from "../assets/typescript.png";
+import playwright from "../assets/playwright.png";
+import Cypress from "../assets/Cypress.png";
 
 function Skills() {
   return (
     <div>
       <PageNav />
-      My Skills
+      <div className={styles.skillsContainer}>
+        {/*<ParticlesBackground />*/}
+        <div className={styles.skillsContent}>
+          <div className={styles.skillsGrid}>
+            {/*
+            <div className={styles.iconContainer}>
+              <FaHtml5 className={styles.icon} title="HTML5" />
+              <FaCss3Alt className={styles.icon} title="CSS3" />
+              <FaJs className={styles.icon} title="JavaScript" />
+              <FaReact className={styles.icon} title="React" />
+            </div>
+          */}
+            <div className={styles.skillsIcons}>
+              <figure className={styles.skillItem}>
+                <img src={html5} alt="HTML5" className={styles.icon} />
+                <figcaption>HTML5</figcaption>
+              </figure>
+              <figure className={styles.skillItem}>
+                <img src={css3} alt="CSS3" className={styles.icon} />
+                <figcaption>CSS3</figcaption>
+              </figure>
+              <figure className={styles.skillItem}>
+                <img src={js} alt="JS" className={styles.icon} />
+                <figcaption>Javascript</figcaption>
+              </figure>
+              <figure className={styles.skillItem}>
+                <img src={jquery} alt="jQuery" className={styles.icon} />
+                <figcaption>jQuery</figcaption>
+              </figure>
+              <figure className={styles.skillItem}>
+                <img src={reactLogo} alt="React" className={styles.icon} />
+                <figcaption>React Js</figcaption>
+              </figure>
+              <figure className={styles.skillItem}>
+                <img src={typescript} alt="TS" className={styles.icon} />
+                <figcaption>Typescript</figcaption>
+              </figure>
+              <figure className={styles.skillItem}>
+                <img
+                  src={playwright}
+                  alt="Playwright"
+                  className={styles.icon}
+                />
+                <figcaption>Playwright</figcaption>
+              </figure>
+              <figure className={styles.skillItem}>
+                <img src={Cypress} alt="Cypress" className={styles.icon} />
+                <figcaption>Cypress</figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
