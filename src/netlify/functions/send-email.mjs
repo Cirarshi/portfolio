07dfb39fee1892a7d43d8974d9855f2c7daeddd4
Utkarsh Kapoor <98.utkarsh.kapoor@gmail.com>
2025-05-27 13:24:16 +0530

    v9
    for mail to work

---
+++ src/netlify/functions/send-email.mjs
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+export async function handler(event, context) {
+  const { name, email, message } = JSON.parse(event.body);
+
+  // configure transporter
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.MY_EMAIL,
+    subject: `Message from ${name}`,
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully" }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: err.message }),
+    };
+  }
+}
